8eec3194f6c72e2ab3e34c5fa3ae9c5364309c14
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-06-29 13:52:16 -0300

    adição do teste do tipo de objeto contido na array de books

--- test/books.test.js
+++ test/books.test.js
@@ -10,12 +10,27 @@ describe("GET /books", () => {
         connection.end();
     })
 
-    it("return 200 and an array of books for correct get", async () => {
+    it("return 200 and an array for correct get", async () => {
         const result = await supertest(app).get("/books");
 
         expect(result.status).toEqual(200);
         expect(result.body).toEqual(
             expect.any(Array)
+        );
+    })
+
+    it("return array of books for correct get", async () => {
+        const result = await supertest(app).get("/books");
+
+        expect(result.body[0]).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                title: expect.any(String),
+                sinopse: expect.any(String),
+                author: expect.any(String),
+                stock: expect.any(Number),
+                price: expect.any(Number)
+            })
         );
     })
 
