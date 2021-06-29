899560d0ae06c03b8692aba56104bd76ab4c78ba
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-06-29 09:53:56 -0300

    rota de livros

--- src/app.js
+++ src/app.js
@@ -7,4 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/books", async (req,res) => {
+    try {
+        const result = await connection.query(`
+            SELECT * FROM books
+        `);
+
+        res.status(200).send(result.rows[0]);
+    } catch(err) {
+        res.status(500).send(err);
+    }
+})
+
 export default app;
--- test/test.js
+++ test/test.js
@@ -1,1 +1,13 @@
-import connection from './database.js'
+import connection from '../src/database.js'
+import app from '../src/app.js'
+import jest from 'jest';
+import supertest from 'supertest';
+
+
+describe("GET /books", () => {
+
+    it("return 200 and an array of books for correct get", async () => {
+
+    })
+
+})
