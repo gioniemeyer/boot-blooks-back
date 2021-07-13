c55ecbd57b6384e26161bd9f7fea27349e89b9fe
thiagoribb <thiribeiro142@gmail.com>
2021-07-13 16:46:22 -0300

    Preparando review

--- src/schemas/SchemaSignUp.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const SchemaSignUp = joi.object({
-    name: joi.string().required(),
-    email: joi.string().email().required(),    
-    password: joi.string().min(4).required()
-});
-
-export { SchemaSignUp };
